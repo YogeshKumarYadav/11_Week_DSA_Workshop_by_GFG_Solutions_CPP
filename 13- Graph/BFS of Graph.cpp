9cd8b93f7e8b2bfea228fa883ed48bb5efa4307a
YogeshKumarYadav--git <001yadavrahul100@gmail.com>
2021-09-21 02:11:50 +0530

    'master'

--- 13- Graph/BFS of Graph.cpp
+++ 13- Graph/BFS of Graph.cpp
@@ -1,63 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
-
- // } Driver Code Ends
-
-
-
-class Solution
-{
-    public:
-    //Function to return Breadth First Traversal of given graph;
     
-	vector<int>bfsOfGraph(int v, vector<int> adj[])
-	{
-	    vector<int> ans;
-	    vector<bool> vis(v, false);
-	    queue<int> q;
-	    
-	    q.push(0);
-	    vis[0] = true;
-	    while(!q.empty())
-	    {
-	        int node = q.front();
-	        q.pop();
-	        ans.push_back(node);
-	        for(auto i: adj[node])
-	            if(vis[i] == false)
-	            {   vis[i] = true;
-	                q.push(i);
-	            }
-	    }
-	    return ans;
+vector<int> bfsOfGraph(int v, vector<int> adj[]) {
+	vector<int> ans;
+	vector<bool> vis(v, false);
+	queue<int> q;
+	
+	q.push(0);
+	vis[0] = true;
+	while(!q.empty()) {
+		int node = q.front();
+		q.pop();
+		ans.push_back(node);
+		for(auto i: adj[node])
+			//i ->vector type
+			if(vis[i] == false) {
+				vis[i] = true;
+				q.push(i);
+			}
 	}
-};
+	return ans;
+}
 
-// { Driver Code Starts.
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int V, E;
-    	cin >> V >> E;
+int main() {
+	int V, E;
+	cin >> V >> E;
 
-    	vector<int> adj[V];
-
-    	for(int i = 0; i < E; i++)
-    	{
-    		int u, v;
-    		cin >> u >> v;
-    		adj[u].push_back(v);
-    // 		adj[v].push_back(u);
-    	}
-        // string s1;
-        // cin>>s1;
-        Solution obj;
-        vector<int>ans=obj.bfsOfGraph(V, adj);
-        for(int i=0;i<ans.size();i++){
-        	cout<<ans[i]<<" ";
-        }
-        cout<<endl;
+	vector<int> adj[V];
+	for(int i = 0; i < E; i++) {
+		int u, v;
+		cin >> u >> v;
+		adj[u].push_back(v);
+ 		adj[v].push_back(u);
 	}
+	vector<int> ans = bfsOfGraph(V, adj);
+	for(int i=0;i<ans.size();i++){
+		cout<<ans[i]<<" ";
+	}
+	cout<<endl;
 	return 0;
-}  // } Driver Code Ends
+} 
